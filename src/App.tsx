43b4a8125ad9942e6b4f8d294254299bc2da94ab
Dariel Noel Vila Plagado <darielnoel@gmail.com>
2024-08-31 17:27:02 +0000

    Renaming to KaibanJS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import AgenticJSComponent from "./AgenticJSComponent";
+import KaibanJSComponent from "./KaibanJSComponent";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <AgenticJSComponent />
+        <KaibanJSComponent />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
